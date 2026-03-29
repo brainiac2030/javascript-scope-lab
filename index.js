03366e883321ef67492a06fb83fc9b5d76005761
brainiac2030 <isaackiambo@gmail.com>
2026-03-29 16:02:59 +0300

    using const instead of var in the burgers variable

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your solution in this file!
-var burgers = ['Hamburger', 'Cheeseburger'];
+const burgers = ['Hamburger', 'Cheeseburger'];
 var featuredDrink = 'Strawberry Milkshake';
 
 function addBurger() {
